4f5c9e7eaa11814662139a664bff362c94a51925
pastoral <munir.hoque@gmail.com>
2018-07-28 12:32:08 +0600

    initial commit

---
+++ Assets/Scripts/ADS_Unity.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ADS_Unity : MonoBehaviour {
+
+	private int coinsc2;
+	private int BookCount, EmeraldCount, PipeCount, GoldCount;
+
+
+	[SerializeField] string gameID = "2704863";
+    public GameObject buttonBon;
+    public Text bonusText;
+	private string bonusName;
+	private int bonuseNumber;
+	private bool generated;
+    private bool freecoin;
+
+	void Awake()
+	{
+		Advertisement.Initialize (gameID, false);
+		generated = false;
+	}
+    
+    void Start()
+    {
+        freecoin = false;
+        if (ProtectedPrefs.HasKey("Coins"))
+        {
+            coinsc2 = ProtectedPrefs.GetInt("Coins");
+        }
+        else {
+            coinsc2 = 0;
+        }
+        BookCount = ProtectedPrefs.GetInt("mBook");
+        GoldCount = ProtectedPrefs.GetInt("mGold");
+        EmeraldCount = ProtectedPrefs.GetInt("mEmerald");
+        PipeCount = ProtectedPrefs.GetInt("mPipe");
+        bonuseNumber = Random.Range(0, 7);
+        switch (bonuseNumber)
+        {
+            case 1:
+                bonusName = "Take 250 coins";
+                break;
+            case 2:
+                bonusName = "Take 3 books";
+                break;
+            case 3:
+                bonusName = "Take 2 pipes";
+                break;
+            case 4:
+                bonusName = "Take 4 emeralds";
+                break;
+            case 5:
+                bonusName = "Take 2 golds";
+                break;
+            default:
+                bonusName = "Take 100 coins";
+                break;
+
+        }
+    }
+	
+	public void ShowAd(string zone = "")
+	{
+        #if UNITY_EDITOR
+          StartCoroutine(WaitForAd());
+        #endif
+
+        if (string.Equals(zone, ""))
+            zone = null;
+
+        ShowOptions options = new ShowOptions();
+        options.resultCallback = AdCallbackhandler;
+
+        if (Advertisement.IsReady(zone))
+            Advertisement.Show(zone, options);
+    }
+	
+	void AdCallbackhandler (ShowResult result)
+	{
+		switch(result)
+		{
+		case ShowResult.Finished:
+			Debug.Log ("Ad Finished. Rewarding player...");
+                if (freecoin)
+                {
+                    ProtectedPrefs.SetInt("Coins", ProtectedPrefs.GetInt("Coins") + 500);
+                    freecoin = false;
+                }
+                else
+                {
+                    InstansShow();
+                }
+			
+			break;
+		case ShowResult.Skipped:
+			Debug.Log ("Ad skipped. Son, I am dissapointed in you");
+			break;
+		case ShowResult.Failed:
+			Debug.Log("I swear this has never happened to me before");
+			break;
+		}
+	}
+	
+	IEnumerator WaitForAd()
+	{
+        float currentTimeScale = Time.timeScale;
+        Time.timeScale = 0f;
+        yield return null;
+
+        while (Advertisement.isShowing)
+            yield return null;
+
+        Time.timeScale = currentTimeScale;
+    }
+
+	void Update(){
+		if (Controller.iDie && !generated){
+			buttonBon.SetActive (true);
+			generated = true;
+			bonusText.text = Advertisement.IsReady() ? bonusName : "No bonus :(";
+		}
+
+	}
+
+	void InstansShow(){
+		switch(bonuseNumber){
+		case 1:
+			ProtectedPrefs.SetInt ("Coins", coinsc2 + 250);
+			break;
+		case 2:
+			ProtectedPrefs.SetInt("mBook", BookCount + 3);
+			break;
+		case 3:
+			ProtectedPrefs.SetInt("mPipe", PipeCount + 2);
+			break;
+		case 4:
+			ProtectedPrefs.SetInt("mEmerald", EmeraldCount+4);
+			break;
+		case 5:
+			ProtectedPrefs.SetInt("mGold", GoldCount +2);
+			break;
+		default:
+			ProtectedPrefs.SetInt ("Coins", coinsc2 + 100);
+			break;
+			
+		}
+		buttonBon.SetActive (false);
+		ProtectedPrefs.Save ();
+	}
+
+    public void GetCoin() {
+        freecoin = true;
+    }
+
+}
---
+++ Assets/Scripts/admobIn.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AdMobPlugin))]
+public class admobIn : MonoBehaviour {
+
+	private const string AD_UNIT_ID = "ca-app-pub-4365083222822400/5806288230";
+	private const string INTERSTITIAL_ID = "ca-app-pub-4365083222822400/1421579847";
+	private AdMobPlugin admob;
+	private bool isShow;
+    private int adNumber;
+	void Start() {
+        adNumber = Random.Range(0, 2);
+        admob = GetComponent<AdMobPlugin>();
+		admob.CreateBanner(adUnitId: AD_UNIT_ID,
+			adSize: AdMobPlugin.AdSize.SMART_BANNER,
+			isTopPosition: true,
+			interstitialId: INTERSTITIAL_ID,
+			isTestDevice: false);
+		admob.RequestAd();
+        #if !UNITY_EDITOR
+		    if(adNumber == 0) admob.RequestInterstitial();
+        #endif
+    }
+
+    void OnEnable() {
+		AdMobPlugin.AdClosed += () => { Debug.Log ("AdClosed"); };
+		AdMobPlugin.AdFailedToLoad += () => { Debug.Log ("AdFailedToLoad"); };
+		AdMobPlugin.AdLeftApplication += () => { Debug.Log ("AdLeftApplication"); };
+		AdMobPlugin.AdOpened += () => { Debug.Log ("AdOpened"); };
+
+		AdMobPlugin.InterstitialClosed += () => { Debug.Log ("InterstitialClosed"); };
+		AdMobPlugin.InterstitialFailedToLoad += () => { Debug.Log ("InterstitialFailedToLoad"); };
+		AdMobPlugin.InterstitialLeftApplication += () => { Debug.Log ("InterstitialLeftApplication"); };
+		AdMobPlugin.InterstitialOpened += () => { Debug.Log ("InterstitialOpened"); };
+
+		AdMobPlugin.InterstitialLoaded += () => { isShow = false; };
+    }
+
+		
+
+	void Update(){
+		if(isShow == false && GameControll.showAd) {
+            if (adNumber == 0) {
+                admob.ShowInterstitial();
+                Debug.Log("Bunner!!");
+            } 
+            isShow = true;
+		} 
+	}
+
+}
